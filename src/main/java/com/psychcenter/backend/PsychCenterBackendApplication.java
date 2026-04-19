dae510d17c36a1980439cbd2096929419be1fbb3
Nur.G <nuray.guliyeva.41@gmail.com>
2026-04-19 23:13:36 +0400

    initial project setup with gradle

---
+++ src/main/java/com/psychcenter/backend/PsychCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.psychcenter.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychCenterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychCenterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psychcenter/backend/PsychCenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psychcenter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychCenterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
