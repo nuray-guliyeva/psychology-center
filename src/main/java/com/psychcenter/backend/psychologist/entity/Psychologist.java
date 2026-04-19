503ee034da02cef59c0520dd1d58fb58e0cd7c43
Nur.G <nuray.guliyeva.41@gmail.com>
2026-04-20 01:17:40 +0400

    feat: add Psychologist entity

---
+++ src/main/java/com/psychcenter/backend/psychologist/entity/Psychologist.java
@@ -0,0 +1,30 @@
+package com.psychcenter.backend.psychologist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "psychologists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Psychologist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String specialization;
+
+    private Integer experienceYears;
+
+    private String email;
+
+    private String phone;
+}
